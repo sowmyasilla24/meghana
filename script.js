31a1c4f0bf2aa19a243acf78c313eebbf555d716
sowmyasilla24 <sowmyasilla2005@gmail.com>
2025-08-07 10:47:07 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('learnMore').addEventListener('click', function() {
-    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
-});
-
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Thank you for your message!');
-    this.reset();
-});
